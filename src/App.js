7d3619599341bd9646ae87538a00b36f2154539f
Vladimir Larin <lvv0820@gmail.com>
2023-01-28 22:59:02 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Phonebook from 'Modules/Phonebook/Phonebook';
+
+export function App() {
+  return (
+    <>
+      <h4>Hi</h4>
+      <Phonebook />
+    </>
+  );
+}
---
+++ src/Modules/Phonebook/Phonebook.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './phonebook.scss';
+
+class Phonebook extends Component {
+ render( ) {
+
+ }
+  return (
+
+  )
+}
+
+export default Phonebook;
