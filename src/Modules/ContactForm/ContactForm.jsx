8ae11e609aa2a1e637f6286241107164933aa107
Vladimir Larin <lvv0820@gmail.com>
2023-01-30 01:40:08 +0200

    ref  start

--- src/Modules/ContactForm/ContactForm.jsx
+++ src/Modules/ContactForm/ContactForm.jsx
@@ -107,8 +107,11 @@ class ContactForm extends Component {
               value={number}
               onChange={handleChange}
               placeholder="+012 123-4567"
-              type="tel"
               className={styles.input}
+              type="tel"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
             />
           </div>
           <button className={styles.btn} type="submit">
